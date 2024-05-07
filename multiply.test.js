991a390d81208322f3a913742a6de5b1fe543cb8
jeevma <sanjiv.matthur@gmail.com>
2024-05-07 14:25:12 -0400

    added test using common matchers

--- multiply.test.js
+++ multiply.test.js
@@ -1,7 +1,17 @@
 const multiply = require('./multiply');
 
-describe('multiply', () => {
-    it ('multiplies two numbers', () => {
+describe('multiply test', () => {
+    it ('multiply 2*3 should equal to 6', () => {
         expect(multiply(2, 3)).toBe(6);
+    });
+
+    test('object example', ()=>{
+        const data = {first: 1}
+    data['second'] = 2;
+    expect(data).toEqual({first: 1, second: 2});
+    });
+
+    it ('multiply 4*3 should not equal to 11', () => {
+        expect(multiply(4, 3)).not.toBe(11);
     });
 });
